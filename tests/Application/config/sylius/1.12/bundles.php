af5ce11e6c6a106dd31a72b5bf75d581a0026634
christopherheroo <christopherheroo@gmail.com>
2023-02-23 04:31:49 +0100

    gha

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
-    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
 ];
