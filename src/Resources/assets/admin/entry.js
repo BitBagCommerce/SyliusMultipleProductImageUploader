dba9e0bb8ba8e4a12c08b7fa96903695083e8877
Krzysztof Odważny <christopherheroo@gmail.com>
2025-04-18 15:01:59 +0200

    add webpack + readme update

---
+++ src/Resources/assets/admin/entry.js
@@ -0,0 +1,2 @@
+import './css/media.css'
+import './js/stimulus.js'
--- tests/Application/webpack.config.js
+++ tests/Application/webpack.config.js
@@ -1,21 +1,41 @@
 const Encore = require('@symfony/webpack-encore');
 const path = require('path');
+const [ bitbagMultipleProductImageUploaderAdmin ] = require('./../../webpack.config.js');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-const syliusBundles = path.resolve(__dirname, 'vendor/sylius/sylius/src/Sylius/Bundle/');
+const syliusBundles = path.resolve(__dirname, '../../vendor/sylius/sylius/src/Sylius/Bundle/');
 const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
 const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
+
+// Shop config
+
+Encore
+  .setOutputPath('public/build/shop/')
+  .setPublicPath('/build/shop')
+  .addEntry('shop-entry', './../../vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/private/entry.js')
+  .disableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enableSassLoader();
+
+const shopConfig = Encore.getWebpackConfig();
+
+shopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
+shopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
+shopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
+shopConfig.name = 'shop';
+
+Encore.reset();
 
 // Admin config
 Encore
   .setOutputPath('public/build/admin/')
   .setPublicPath('/build/admin')
-  .addEntry('image-uploader-entry', './assets/admin/js/entry.js')
-  .addStyleEntry('image-uploader-style-entry', './assets/admin/css/media.css')
-  .enableStimulusBridge('./assets/admin/js/controllers.json')
+  .addEntry('admin-entry', './../../vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/entry.js')
   .disableSingleRuntimeChunk()
   .splitEntryChunks()
   .enableSingleRuntimeChunk()
@@ -27,10 +47,13 @@ Encore
 
 const adminConfig = Encore.getWebpackConfig();
 
+adminConfig.resolve.alias['chart.js/dist/Chart.min'] = path.resolve(__dirname, 'node_modules/chart.js/dist/chart.min.js');
 adminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
 adminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 adminConfig.externals = Object.assign({}, adminConfig.externals, { window: 'window', document: 'document' });
 adminConfig.name = 'admin';
 
-module.exports = [adminConfig];
+Encore.reset();
+
+module.exports = [shopConfig, adminConfig, bitbagMultipleProductImageUploaderAdmin];
